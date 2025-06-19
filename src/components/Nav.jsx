b63fcedab4d92dfb9f728b61445e751e6f26b9d9
Noah <noahjames1035@gmail.com>
2025-06-19 14:38:16 -0400

    Nav UI complete

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import myLogo from "../assets/mylogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Nav() {
+  return (
+    <>
+      <div id="section">
+        <div className="container">
+          <div className="row">
+            <figure className="nav__img--wrapper">
+              <img src={myLogo} alt="" className="nav__img" />
+            </figure>
+            <ul className="nav__link--list">
+              <li className="nav__link">
+                <a href="#gallery">Gallery</a>
+              </li>
+              <li className="nav__link">
+                <a href="#projects">Projects</a>
+              </li>
+              <li className="nav__link">
+                <a href="#contact">Contact</a>
+              </li>
+              <div className="dark-or-light btn">
+                <FontAwesomeIcon icon="circle-half-stroke" className="dark-or-light__img" />
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
