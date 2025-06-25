4c76d2809cc1877f1885b7f57aa422f3d5c8ee3d
Noah <noahjames1035@gmail.com>
2025-06-25 11:39:54 -0400

    Project 1 added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
+import Library from "../assets/Library.png";
 
 function Projects() {
   return (
     <div>
-        <div id="section">
-            <div className="container projects__container">
-                <div className="section__title">
-                    Projects
-                </div>
+      <div id="section">
+        <div className="container projects__container">
+          <div className="section__title">Projects</div>
+          <div className="project__info">
+            <div className="project__wrapper">
+              <div className="project__title">Library</div>
+              <div className="project__description"></div>
+              <img src={Library} alt="" className="project__img" />
             </div>
+            <div className="project__wrapper">
+              <div className="project__title">Skinstric A.I</div>
+              <img src="" alt="" className="project__img" />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -4,55 +4,66 @@ import TailwindCSS from "../assets/TailwindCSS.png";
 import NextJS from "../assets/NextJS.png";
 import Redux from "../assets/Redux.png";
 
-
 function Technology() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
     <>
       <div id="section tech__section">
         <div className="container tech__container">
           <div className="section__title">Technology</div>
-            <div className="tech__list--wrapper">
-              <div className="technology__title">HTML</div>
+          <div className="tech__list--wrapper">
+            <div className="tech__info">
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/240px-HTML5_Badge.svg.png"
                 alt=""
-                className="technology__img"
+                className="technology__img html"
               />
-              <div className="technology__title">CSS</div>
+              <div className="technology__title">HTML</div>
+            </div>
+            <div className="tech__info">
               <img
                 src="https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png"
                 alt=""
-                className="technology__img"
+                className="technology__img css"
               />
-              <div className="technology__title">JavaScript</div>
+              <div className="technology__title">CSS</div>
+            </div>
+            <div className="tech__info">
               <img
                 src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png"
                 alt=""
-                className="technology__img"
+                className="technology__img javascript"
+              />
+              <div className="technology__title">JavaScript</div>
+            </div>
+            <div className="tech__info">
+              <img
+                src="https://cdn.iconscout.com/icon/free/png-256/react-3-1175109.png"
+                alt=""
+                className="technology__img react"
               />
               <div className="technology__title">React</div>
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-256/react-3-1175109.pngF"
-                alt=""
-                className="technology__img"
-              />
-              <div className="technology__title">Git</div>
-              <img src={Git} alt="" className="technology__img" />
-              <div className="technology__title">Tailwind</div>
-              <img src={TailwindCSS} alt="" className="technology__img" />
-              <div className="technology__title">Next JS</div>
-              <img src={NextJS} alt="" className="technology__img" />
-              <div className="technology__title">Redux</div>
-              <img src={Redux} alt="" className="technology__img" />
             </div>
+            <div className="tech__info">
+              <img src={Git} alt="" className="technology__img git" />
+              <div className="technology__title">Git</div>
+            </div>
+            <div className="tech__info">
+              <img
+                src={TailwindCSS}
+                alt=""
+                className="technology__img tailwind"
+              />
+              <div className="technology__title">Tailwind</div>
+            </div>
+            <div className="tech__info">
+              <img src={NextJS} alt="" className="technology__img nextjs" />
+              <div className="technology__title">Next JS</div>
+            </div>
+            <div className="tech__info">
+              <img src={Redux} alt="" className="technology__img redux" />
+              <div className="technology__title">Redux</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
